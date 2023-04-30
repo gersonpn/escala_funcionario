2c003d37701a00e65535156694030f67aca4a9e3
gersonpn <gerson.neto@estudante.ifms.edu.br>
2023-04-30 00:31:53 -0400

    cadastro, recuperação de senha

--- js/main.js
+++ js/main.js
@@ -7,8 +7,44 @@ const url = `https://calendarific.com/api/v2/holidays?api_key=${api_key}&country
 fetch(url)
   .then(response => response.json())
   .then(data => {
-    
+
   })
   .catch(error => {
     console.error('Erro na solicitação:', error);
   });
+
+
+  function cadastrar() {
+    var nome = document.getElementById('inputNome').value;
+    var sobrenome = document.getElementById('inputSobrenome').value;
+    var email = document.getElementById('cdEmail').value;
+    var senha = document.getElementById('cdPassword').value;
+    var confirmarSenha = document.getElementById('confirmPassword').value;
+
+   
+    if (senha != confirmarSenha) {
+      alert('As senhas não coincidem!');
+      return;
+    }
+
+
+    var usuario = {
+      nome: nome,
+      sobrenome: sobrenome,
+      email: email,
+      senha: senha
+    };
+
+     axios.post('/api/usuarios', usuario) .then(function (response) {
+      alert('Usuário cadastrado com sucesso!');
+        window.location.href = '/login.html';
+     })
+       .catch(function (error) {
+         alert('Ocorreu um erro ao cadastrar o usuário!');
+         console.log(error);
+       });
+
+
+    alert('Usuário cadastrado com sucesso!');
+    window.location.href = '/login.html';
+  }
